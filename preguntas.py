c7874d00b66f583a6c28963a36aa465a79f232da
JuanPabloArcilaLopez <112211363+JuanPabloArcilaLopez@users.noreply.github.com>
2022-09-22 21:05:49 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -21,10 +21,12 @@ def pregunta_01():
     214
 
     """
-    X=open("./data.csv","r").readlines()
-    
-    return
-
+    x=open("./data.csv","r").readlines()
+    x=[z.replace("\n",""] for z in x
+    x=[z.split("\t"] for z in x
+    column2=[int(x[1]) for x in x]
+    listsum=sum(column2)
+    return listsum
 
 def pregunta_02():
     """
