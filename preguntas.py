d8f33c1ae7cb7bfb704f1f2756a477c05000ebc4
JuanPabloArcilaLopez <112211363+JuanPabloArcilaLopez@users.noreply.github.com>
2022-09-24 14:02:02 -0500

    Intento Taller completo

--- preguntas.py
+++ preguntas.py
@@ -21,15 +21,12 @@ def pregunta_01():
     214
 
     """
-    x=open("./data.csv","r").readlines()
-    x=[z.replace("\n","") for z in x]            
-    x=[z.split("\t") for z in x]
-    column2=[int(x[1]) for x in x]
-    listsum=sum(column2)
-    print (listsum)
-    return listsum
-
-
+    x = open("./data.csv", "r").readlines()
+    x = [z.replace("\n", "") for z in x]
+    x = [z.split("\t") for z in x]
+    column2=[int(z[1]) for z in x ]
+    listSum = sum(column2)
+    return listSum
 
 def pregunta_02():
     """
@@ -46,9 +43,10 @@ def pregunta_02():
     ]
 
     """
-    x=open("./data.csv","r").readlines()
-    column1=[x[0] for x in x]
-    listatuple=[tuple([x,column1.count(x)]) for x in set(column1)]
+    x = open("./data.csv", "r").readlines()
+    column1=[x[0] for x in x ]
+    print (column1)
+    listatuple =[tuple([x,column1.count(x)]) for x in set(column1)]
     listatuple.sort()
     return listatuple
 
@@ -68,7 +66,6 @@ def pregunta_03():
     ]
 
     """
-    x=open("./data.csv","r").readlines()
     with open('data.csv', 'r') as f:
         lines = f.readlines()
         rows = [line.split('\t') for line in lines]
@@ -79,11 +76,11 @@ def pregunta_03():
         for i in x:
             if i[0] not in d.keys():
                 d[i[0]] = int(i[1])
-            else:
-                d[i[0]] = d[i[0]] + int(i[1])
+            else:   
+                d[i[0]] = d[i[0]] + int(i[1])      
     list = [(k, v) for k, v in d.items()]
     list.sort()
-    return list.sort
+    return list
 
 
 def pregunta_04():
@@ -108,6 +105,13 @@ def pregunta_04():
     ]
 
     """
+    with open('data.csv', 'r') as f:
+        lines = f.readlines()
+        rows = [line.split('\t') for line in lines]
+        col3 = [c[2] for c in rows]
+        months = [k.split('-')[1] for k in col3]
+        listatuple =[tuple([i,months.count(i)])for i in sorted (set(months))]
+    return listatuple
 
 
 def pregunta_05():
@@ -125,7 +129,18 @@ def pregunta_05():
     ]
 
     """
-    return 
+    with open('data.csv', 'r') as f:
+        lines = f.readlines()
+        rows = [line.split('\t') for line in lines]
+        x = [c[0:2] for c in rows]
+        d = {}
+        for i in x:
+            if i[0] not in d.keys():
+                d[i[0]] = [int(i[1])]
+            else:
+                d[i[0]].append(int(i[1]))
+    listatuple=[tuple([i,max(d[i]),min(d[i])]) for i in d.keys()]
+    return listatuple
 
 
 def pregunta_06():
@@ -150,7 +165,25 @@ def pregunta_06():
     ]
 
     """
-    return
+    with open('data.csv', 'r') as f:
+        lines = f.readlines()
+        rows = [line.split('\t') for line in lines]    
+        x = [c[4].split(',') for c in rows]
+        d = {}
+        for l in x:
+            for e in l:
+                if e[:3] not in d.keys():
+                    r=e[:].split(":")
+                    r=r[1]
+                    d[e[:3]] = [int(r)]
+                                
+                else:
+                    r=e[:].split(":")
+                    r=r[1]
+                    d[e[:3]].append(int(r))
+    listatuple =[tuple([i,min(d[i]),max(d[i])]) for i in d.keys()]
+    listatuple.sort()
+    return listatuple
 
 
 def pregunta_07():
@@ -174,7 +207,18 @@ def pregunta_07():
     ]
 
     """
-    return
+    with open('data.csv', 'r') as f:
+        lines = f.readlines()
+        rows = [line.split('\t') for line in lines]
+        x = [c[:2] for c in rows]
+        d = {}
+        for i in x:
+            if i[1] not in d.keys():
+                d[i[1]] = [i[0]]
+            else:
+                d[i[1]].append(i[0])
+    listatuple=[tuple([k,d[k]]) for k in sorted(d.keys())]
+    return listatuple
 
 
 def pregunta_08():
@@ -199,7 +243,18 @@ def pregunta_08():
     ]
 
     """
-    return
+    with open('data.csv', 'r') as f:
+        lines = f.readlines()
+        rows = [line.split('\t') for line in lines]
+        x = [c[:2] for c in rows]
+        d = {}
+        for i in x:
+            if i[1] not in d.keys():
+                d[i[1]] = [i[0]]
+            else:
+                d[i[1]].append(i[0])
+    listatuple=[tuple([k,sorted(set(d[k]))]) for k in sorted(d.keys())]
+    return listatuple
 
 
 def pregunta_09():
@@ -222,7 +277,13 @@ def pregunta_09():
     }
 
     """
-    return
+    with open('data.csv', 'r') as f:
+        lines = f.readlines()
+        rows = [line.split('\t') for line in lines]    
+        x = [c[4].split(',') for c in rows]
+        g = [k[:3] for i in x for k in i]
+    d={l:g.count(l) for l in sorted(set(g))}
+    return d
 
 
 def pregunta_10():
@@ -243,7 +304,13 @@ def pregunta_10():
 
 
     """
-    return
+    with open('data.csv', 'r') as f:
+        lines = f.readlines()
+        rows = [line.split('\t') for line in lines]    
+        x = [[c[0], c[3], c[4]] for c in rows]
+
+        listatuple =[tuple([i[0],len(i[1].split(',')),len(i[2].split(','))])for i in x]
+    return listatuple
 
 
 def pregunta_11():
@@ -264,7 +331,19 @@ def pregunta_11():
 
 
     """
-    return
+    with open('data.csv', 'r') as f:
+        lines = f.readlines()
+        rows = [line.split('\t') for line in lines]    
+        x = [[c[1], c[3].split(',')] for c in rows]
+        d = {}
+        for r in x:        
+            for l in r[1]:
+                if l not in d.keys():
+                    d[l] = [int(r[0])]
+                else:
+                    d[l].append(int(r[0]))
+    d={i:sum(d[i]) for i in sorted(d.keys())}
+    return d
 
 
 def pregunta_12():
@@ -282,4 +361,22 @@ def pregunta_12():
     }
 
     """
-    return
+    import re
+    d = {}
+    Lista=[]
+    with open('data.csv', 'r') as f:
+        lines = f.readlines()
+        rows = [line.split('\t') for line in lines]    
+        x = [[c[0], c[4]] for c in rows]
+        for k in x:
+            num = re.findall('\d+', k[1])
+            num = [int(i) for i in num]
+            Lista+=[[k[0],sum(num)]]
+        for i in Lista:
+            if i[0] not in d.keys():
+                d[i[0]] = int(i[1])
+            else:
+                d[i[0]] = d[i[0]] + int(i[1])
+                
+    d=dict(sorted(d.items()))
+    return d
